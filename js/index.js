30715b7da2cb84b9a4ed1f295fb2384b27853e12
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2024-04-10 13:10:50 +0800

    Adjusted layout and font to match outcome

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ console.log(data)
 //getting all h3
 const summaryTitles = [...document.querySelectorAll('.stat h3')]
 const summaryTitlesIcons = [...document.querySelectorAll('.stat img')]
-const summaryTitlesScores = [...document.querySelectorAll('.stat p')]
+const summaryTitlesScores = [...document.querySelectorAll('.stat span')]
 
 //populating all h3
 data.forEach((d, index) => {
     summaryTitles[index].innerText = d.category
     summaryTitlesIcons[index].src = d.icon
-    summaryTitlesScores[index].innerText = `${d.score} / 100`
+    summaryTitlesScores[index].innerText = d.score
 })
