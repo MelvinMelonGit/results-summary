ccde8ff1c08b783d2ae501c722cef3537c7586a8
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2024-04-10 12:50:37 +0800

    Edited layout

---
+++ js/index.js
@@ -0,0 +1,14 @@
+import data from './data.json' with { type: "json" }
+console.log(data)
+
+//getting all h3
+const summaryTitles = [...document.querySelectorAll('.stat h3')]
+const summaryTitlesIcons = [...document.querySelectorAll('.stat img')]
+const summaryTitlesScores = [...document.querySelectorAll('.stat p')]
+
+//populating all h3
+data.forEach((d, index) => {
+    summaryTitles[index].innerText = d.category
+    summaryTitlesIcons[index].src = d.icon
+    summaryTitlesScores[index].innerText = `${d.score} / 100`
+})
